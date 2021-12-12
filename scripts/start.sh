c2b7b14653b65e5352ec6ee74d5c0ac95b689e36
Heito <nikita134222@gmail.com>
2021-12-12 21:42:52 +0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,16 +9,18 @@ echo -ne '\033]0;dxh.su\a'
 w="\e[0m\e[1;38m" # white
 g="\e[0;37m" # gray
 
-text="kek"
+text="dxh.su"
 
 preStart()
 {
     for i in {1..$(($2 / 2 - $1))}; do
-        echo -ne " ";
+        echo -n " ";
     done
 }
 
 c=0
+
+# tput sgr0; tput setaf 3
 
 while true; do
     clear
@@ -37,12 +39,12 @@ while true; do
 
     for i in {..$(($tl / 2 - 4))}; do echo "$w "; done
 
-    echo "\n$( preStart 2 $tc ) $r \b__$w"
-    echo "\n$( preStart 4 $tc )  $r'_')$w"
-    echo "\n$( preStart 10 $tc )         $r\(--$w-."
-    echo "\n$( preStart 14 $tc )            $r(( )$w \b___\\"
-    echo "\n$( preStart 11 $tc )      dx $r\bh//-$w\\\\\\ \\\\\\"
-    echo "\n\n$( preStart $(( ${#text} / 2 )) $tc  )$g$text\n"
+    echo "$( preStart 2 $tc ) $r \b__$w"
+    echo "$( preStart 4 $tc )  $r'_')$w"
+    echo "$( preStart 10 $tc )         $r\(--$w-."
+    echo "$( preStart 14 $tc )            $r(( )$w \b___\\"
+    echo "$( preStart 11 $tc )      dx $r\bh//-$w\\\\\\ \\\\\\"
+    echo "\n$( preStart $(( ${#text} / 2 )) $tc  )$g$text\n"
 
     sleep .5
 done
